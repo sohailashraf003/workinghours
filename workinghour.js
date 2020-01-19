304ff8551847626d1f3dd86248aa257918b2bbb7
Sohail Ashraf <me.sohail31@gmail.com>
2020-01-20 03:14:28 +0800

    init

---
+++ workinghour.js
@@ -0,0 +1,77 @@
+/*
+# Objective:
+write a function to find expiry datetime. expiry datetime is 3 working hours from "now".
+the working hours is defined in "schedule" input parameter.
+You can write the function in java or javascript.
+
+# input parameters:
+now: datetime, current datetime. e.g: '2019-10-11T08:13:07+0800'
+schedule: an arraylist of map object. which specified the day open or close and also the start and end of working hours
+*/
+let schedule = [
+  { open: false, open_at: "", close_at: "" }, // sunday -> 0
+  { open: true, open_at: "09:00", close_at: "18:00" }, // monday - 1
+  { open: true, open_at: "09:00", close_at: "18:00" }, // -> 2
+  { open: true, open_at: "09:00", close_at: "18:00" }, // -> 3
+  { open: true, open_at: "09:00", close_at: "18:00" }, // -> 4
+  { open: true, open_at: "09:00", close_at: "17:00" }, // -> 5
+  { open: false, open_at: "", close_at: "" } // - > 6
+];
+
+/*
+# example:
+now is friday 4pm. whith the above schedule, the expiry date should be next monday 10am. because on friday office close
+at 5pm and office is closed on weekend.
+
+output: datetime, 3 working hour from input date ("now")
+*/
+
+function isopen(date, schedule) {
+  return schedule[date.getDay()].open;
+}
+
+function getOpenTime(now, schedule) {
+  let timestamp = new Date(now.getTime());
+  let scheduleDay = schedule[timestamp.getDay()];
+  let open_at = scheduleDay.open_at;
+  timestamp.setHours(parseInt(open_at));
+  return new Date(timestamp);
+}
+
+function getCloseTime(now, schedule) {
+  let timestamp = new Date(now.getTime());
+  let scheduleDay = schedule[timestamp.getDay()];
+  let close_at = scheduleDay.close_at;
+  timestamp.setHours(parseInt(close_at));
+  return new Date(timestamp);
+}
+
+
+function find(now, schedule, expirey) {
+  if (
+    !isopen(now, schedule) ||
+    now.getTime() >= getCloseTime(now, schedule).getTime()
+  ) {
+    let newDate = new Date(now.getTime());
+    newDate.setDate(newDate.getDate() + 1);
+    newDate.setHours(9);
+    return find(newDate, schedule, expirey);
+  }
+
+  let date = new Date(now.getTime() + expirey);
+  if (date.getTime() > getCloseTime(date, schedule).getTime()) {
+    let diff = date.getTime() - getCloseTime(date, schedule).getTime();
+    let newDate = new Date(now.getTime());
+    newDate.setDate(newDate.getDate() + 1);
+    newDate.setHours(9);
+    return find(newDate, schedule, diff);
+  }
+  return date;
+}
+
+module.exports = {
+  find,
+  getCloseTime,
+  schedule,
+  getOpenTime
+};
