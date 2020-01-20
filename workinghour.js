7d3bde452a4c225d0c76e950aca88f28edc69dd7
Sohail Ashraf <me.sohail31@gmail.com>
2020-01-20 11:42:07 +0800

    changing the name of find function to calculateExpiryDate

--- workinghour.js
+++ workinghour.js
@@ -1,6 +1,6 @@
 /*
 # Objective:
-write a function to find expiry datetime. expiry datetime is 3 working hours from "now".
+write a function to calculateExpiryDate expiry datetime. expiry datetime is 3 working hours from "now".
 the working hours is defined in "schedule" input parameter.
 You can write the function in java or javascript.
 
@@ -10,7 +10,7 @@ schedule: an arraylist of map object. which specified the day open or close and 
 */
 let schedule = [
   { open: false, open_at: "", close_at: "" }, // sunday -> 0
-  { open: true, open_at: "09:00", close_at: "18:00" }, // monday - 1
+  { open: true, open_at: "09:00", close_at: "18:00" }, // monday -> 1
   { open: true, open_at: "09:00", close_at: "18:00" }, // -> 2
   { open: true, open_at: "09:00", close_at: "18:00" }, // -> 3
   { open: true, open_at: "09:00", close_at: "18:00" }, // -> 4
@@ -46,31 +46,40 @@ function getCloseTime(now, schedule) {
   return new Date(timestamp);
 }
 
-
-function find(now, schedule, expirey) {
+/**
+ * Return the expiry datetime
+ * @param {Date} now: Order placement date
+ * @param {*} schedule: Office week schedule (array of object
+ * @param {*} expirey: Expiry date of the order in milliseconds
+ */
+function calculateExpiryDate(now, schedule, expirey) {
   if (
     !isopen(now, schedule) ||
     now.getTime() >= getCloseTime(now, schedule).getTime()
   ) {
     let newDate = new Date(now.getTime());
+    let openAt = parseInt(schedule[newDate.getDay()].open_at);
     newDate.setDate(newDate.getDate() + 1);
-    newDate.setHours(9);
-    return find(newDate, schedule, expirey);
+    if (isNaN(openAt)) newDate.setHours(9);
+    else newDate.setHours(openAt);
+    return calculateExpiryDate(newDate, schedule, expirey);
   }
 
   let date = new Date(now.getTime() + expirey);
   if (date.getTime() > getCloseTime(date, schedule).getTime()) {
     let diff = date.getTime() - getCloseTime(date, schedule).getTime();
     let newDate = new Date(now.getTime());
+    let openAt = parseInt(schedule[newDate.getDay()].open_at);
     newDate.setDate(newDate.getDate() + 1);
-    newDate.setHours(9);
-    return find(newDate, schedule, diff);
+    if (isNaN(openAt)) newDate.setHours(9);
+    else newDate.setHours(openAt);
+    return calculateExpiryDate(newDate, schedule, diff);
   }
   return date;
 }
 
 module.exports = {
-  find,
+  calculateExpiryDate,
   getCloseTime,
   schedule,
   getOpenTime
--- workinghour.test.js
+++ workinghour.test.js
@@ -1,14 +1,20 @@
-const { find, getCloseTime, schedule, getOpenTime } = require("./workinghour");
-
+const {
+  calculateExpiryDate,
+  getCloseTime,
+  schedule,
+  getOpenTime
+} = require("./workinghour");
 
 describe("Utility Functions", () => {
   it("should return the close time to the working day", () => {
+
     let expected =
       "Mon Jan 20 2020 18:00:00 GMT+0800 (Singapore Standard Time)";
     let actual = getCloseTime(
       new Date("2020-01-20T17:00:00+0800"),
       schedule
     ).toString();
+
     expect(actual).toBe(expected);
 
     expected = "Wed Jan 15 2020 18:00:00 GMT+0800 (Singapore Standard Time)";
@@ -16,6 +22,7 @@ describe("Utility Functions", () => {
       new Date("2020-01-15T23:00:00+0800"),
       schedule
     ).toString();
+
     expect(actual).toBe(expected);
   });
 
@@ -26,6 +33,7 @@ describe("Utility Functions", () => {
       new Date("2020-01-20T17:00:00+0800"),
       schedule
     ).toString();
+
     expect(actual).toBe(expected);
 
     expected = "Wed Jan 15 2020 09:00:00 GMT+0800 (Singapore Standard Time)";
@@ -33,36 +41,59 @@ describe("Utility Functions", () => {
       new Date("2020-01-15T23:00:00+0800"),
       schedule
     ).toString();
+
     expect(actual).toBe(expected);
   });
 });
-
 
 describe("Set the expiry datetime", () => {
   let expireyTime = 3 * 60 * 60 * 1000;
 
   it("should return the expiry date with todays date", () => {
-      let expected = 'Tue Jan 21 2020 11:55:07 GMT+0800 (Singapore Standard Time)';
-      let actual = find(new Date("2020-01-20T17:55:07+0800"), schedule, expireyTime).toString();
-      expect(actual).toBe(expected);
+    let expected =
+      "Tue Jan 21 2020 11:55:07 GMT+0800 (Singapore Standard Time)";
+    let actual = calculateExpiryDate(
+      new Date("2020-01-20T17:55:07+0800"),
+      schedule,
+      expireyTime
+    ).toString();
+
+    expect(actual).toBe(expected);
   });
 
   it("should return the expiry date to next working day", () => {
-    let expected = 'Mon Jan 20 2020 12:00:07 GMT+0800 (Singapore Standard Time)';
-    let actual = find(new Date("2020-01-19T11:00:07+0800"), schedule, expireyTime).toString();
+    let expected =
+      "Mon Jan 20 2020 12:00:07 GMT+0800 (Singapore Standard Time)";
+    let actual = calculateExpiryDate(
+      new Date("2020-01-19T11:00:07+0800"),
+      schedule,
+      expireyTime
+    ).toString();
+
     expect(actual).toBe(expected);
-})
+  });
 
-it("should return the expiry date on Monday", () => {
-  let expected = 'Mon Jan 20 2020 12:55:07 GMT+0800 (Singapore Standard Time)';
-  let actual = find(new Date("2020-01-18T14:55:07+0800"), schedule, expireyTime).toString();
-  expect(actual).toBe(expected);
-})
+  it("should return the expiry date on Monday", () => {
+    let expected =
+      "Mon Jan 20 2020 12:55:07 GMT+0800 (Singapore Standard Time)";
+    let actual = calculateExpiryDate(
+      new Date("2020-01-18T14:55:07+0800"),
+      schedule,
+      expireyTime
+    ).toString();
 
-it("should add the expiry today and next day", () => {
-  let expected = 'Wed Jan 22 2020 11:30:07 GMT+0800 (Singapore Standard Time)';
-  let actual = find(new Date("2020-01-21T17:30:07+0800"), schedule, expireyTime).toString();
-  expect(actual).toBe(expected);
-})
+    expect(actual).toBe(expected);
+  });
 
+  it("should add the expiry today and next day", () => {
+    let expected =
+      "Wed Jan 22 2020 11:30:07 GMT+0800 (Singapore Standard Time)";
+    let actual = calculateExpiryDate(
+      new Date("2020-01-21T17:30:07+0800"),
+      schedule,
+      expireyTime
+    ).toString();
+    
+    expect(actual).toBe(expected);
+  });
 });
